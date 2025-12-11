014f9f2ab25b4a5ac607808cbea02c4ba65d1cdc
Bennett Lundberg <bennett.lundberg@milliman.com>
2025-12-11 16:55:59 -0600

    ETL - Updates to Batting/Pitching Seasonal Logs

--- ETL/App/MLBExtractor/Lib/qPitching.py
+++ ETL/App/MLBExtractor/Lib/qPitching.py
@@ -3,16 +3,14 @@ import pybaseball
 import pandas as pd
 
 
-class Batting:
+class Pitching:
 
     @staticmethod
     def getSeasonal():
 
         dat = pd.DataFrame()
         for season in range(1950, 2026, 1):
-            iter = pybaseball.batting_stats(season)
+            iter = pybaseball.pitching_stats(season)
             dat = pd.concat([dat, iter], axis = 1)
 
         return dat
-    
-    
--- ETL/App/MLBExtractor/Program.py
+++ ETL/App/MLBExtractor/Program.py
@@ -1,14 +1,40 @@
 
 from Lib.qBatting import Batting
+from Lib.qPitching import Pitching
 
 import pandas as pd
+import json
+import os
+import pyarrow
 
+with open(f'{os.path.dirname(os.path.abspath(__file__))}\\Properties\\launchSettings.json', 'r') as f:
+    args = json.load(f)
 
-def main():
+def main(args = args):
 
-    extractor = Batting()
-    seasonal = extractor.seasonal()
-    print(seasonal)
+    ### Read arguments
+    localContainer = args['container']
+
+    ### Source Data; Create DataFrame
+    battingExtractor = Batting()
+    battingSeasonal = battingExtractor.getSeasonal()
+
+    ## Pitching
+    pitchingExtractor = Pitching()
+    pitchingSeasonal - picthingExtractor.getSeasonal()
+
+    ### Assign pandas objects to table
+    tablePairs = [
+        (battingSeasonal, 'StagingMLBOffensiveSeasonalLog'),
+        (pitchingSeasonal, 'StagingMLBPitchingSeasonalLog')
+    ]
+
+    ### Iterate through table pairs; write to file
+    for tuple in tablePairs:
+
+        dat: pd.DataFrame = tuple[0]
+        dat.to_parquet(f'{localContainer}{tuple[1]}.parquet', engine = 'pyarrow')
+    
     
 if __name__ == '__main__':
     main()
